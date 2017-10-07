4e1307b510445b47a54424119f82b48d75831195
Habibun Noby <habibun.noby@gmail.com>
2017-10-07 12:52:16 +0600

    Fix problem 2

--- spl/loop/problem-2/solution.c
+++ spl/loop/problem-2/solution.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int i, j, k, n = 4;
 
     for(i = 1; i <= n; i++){
         printf("%d", i);
-        for(j = 1; j <= n-1; j++){
+        for(j = 1; j <= n-i; j++){
             printf(" ");
         }
 
         for(k = 1; k <= i; k++){
             printf("*");
         }
+
         printf("\n");
     }
 }
