8241d4c5fe035c37c9ddb75a92bd92acd4097ec5
Habibun Noby <habibun.noby@gmail.com>
2017-10-08 20:40:17 +0600

    Continue assignment

--- spl/loop/assignment/solution.c
+++ spl/loop/assignment/solution.c
@@ -9,8 +9,8 @@ int main()
 
     for(i = 1; i <= n; i++){
 
-        for(j = 0; j <= n-i; j++){
-            printf(" ");
+        for(j = 1; j < i; j++){
+            printf("  ");
         }
 
         for(j = 0; j <= n - i; j++){
@@ -20,7 +20,11 @@ int main()
         printf("\n");
     }
 
-    for(i = 1; i < n; i++){
+    for(i = 1; i <= n; i++){
+        for(j = 2; j <= n - i; j++){
+            printf("  ");
+        }
+
         for(j = 0; j <= i; j++){
             printf("* ");
         }
