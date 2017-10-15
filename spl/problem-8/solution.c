3351de9c7cfa0ebf63435671aadcc41b4b7af708
Habibun Noby <habibun.noby@gmail.com>
2017-10-15 20:44:19 +0600

    Solve find maximum or minimum element in array

--- spl/problem-8/solution.c
+++ spl/problem-8/solution.c
@@ -2,14 +2,36 @@
 
 int main()
 {
-    int number;
+    int arr[100];
+    int i, max, min, size;
 
-    printf("Enter an integer: ");
-    scanf("%d", &number);
+    /* Input size of the array */
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
 
-    if(number % 2 == 0){
-        printf("%d is even.", number);
-    }else{
-        printf("%d is odd.", number);
+    /* Input array elements */
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++){
+        scanf("%d", &arr[i]);
     }
+
+    /* Assume first element as maximum and minimum */
+    max = arr[0];
+    min = arr[0];
+
+    /* Find maximum maximum and minimum in all array  elements */
+    for(i=0; i<size; i++){
+        /* If current element of array is greater than max */
+        if(arr[i] > max){
+            max = arr[i];
+        }
+
+        /* If current element of array is greater than max */
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d", min);
 }
