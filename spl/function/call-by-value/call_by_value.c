235af706d02f546ce07c29584b1fe4413d506cfb
Habibun Noby <habibun.noby@gmail.com>
2017-11-25 21:21:26 +0600

    Solve call by value and call by reference

---
+++ spl/function/call-by-value/call_by_value.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void changeValue(int x);
+
+int main()
+{
+    int x=5;
+
+    printf("Before function call: %d \n", x);
+    changeValue(x);
+    printf("After function call: %d \n", x);
+
+    return 0;
+}
+
+void changeValue(int x)
+{
+    x = 10;
+    printf("In function: %d \n", x);
+}
