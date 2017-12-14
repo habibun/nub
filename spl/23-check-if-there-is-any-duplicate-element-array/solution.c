22087c9a08f4ed557bab4f51cf92c3c586ebe0f3
Habibun Noby <habibun.noby@gmail.com>
2017-12-14 21:10:31 +0600

    Solve duplicate element in array

--- spl/23-check-if-there-is-any-duplicate-element-array/solution.c
+++ spl/23-check-if-there-is-any-duplicate-element-array/solution.c
@@ -1,21 +1,20 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main()
 {
-    int i, j, k, n, arr[n];
-
-    printf("enter the array size \n");
+    int n, i, j, ara[n];
+    printf("Enter array size: ");
     scanf("%d", &n);
 
-    for(i=0; i<n; i++){
-        scanf("%d", &arr[i]);
+    printf("Enter array values: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &ara[i]);
     }
 
-    for(k=0; k< n; k++){
-        for(j=0 + 1; j<n; j++){
-            if(arr[j] == arr[k]){
-                printf("Duplicate array found.");
-
+    for(i = 0; i < n; i++){
+        for(j = i+1; j < n; j++){
+            if(ara[i] == ara[j]){
+                printf("Duplicate element found.");
                 return 0;
             }
         }
