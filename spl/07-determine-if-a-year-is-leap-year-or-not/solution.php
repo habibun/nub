3c5dc89b8f3975f83045b217a51c171072d41f2b
UdaySarker <34117689+UdaySarker@users.noreply.github.com>
2018-01-24 15:28:58 +0600

    new logic applied

--- spl/07-determine-if-a-year-is-leap-year-or-not/solution.php
+++ spl/07-determine-if-a-year-is-leap-year-or-not/solution.php
@@ -1,10 +1,19 @@
 <?php 
 	$year=readline("Enter Year: \n");
-	
-	if($year%4==0 or $year%100==0 or $year%400==0){
-		echo $year ." is a Leap Year";
+	if($year%4==0){
+		if($year%100==0){
+			if($year%400==0){
+				echo "$year is a leap year";
+			}
+				else{
+					echo "$year is not a leap year";
+				}
+		}
+			else{
+				echo "$year is a leap year";
+			}
 	}
-	else{
-		echo $year. " is not a Leap Year";
-	}
+		else{
+			echo "$year is not a leap year";
+		}
  ?>
