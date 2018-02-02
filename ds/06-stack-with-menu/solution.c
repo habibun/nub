14ae8f7644800c45ab1a06c5cba8acb27d9cd3fe
Habibun Noby <habibun.noby@gmail.com>
2018-02-02 22:43:16 +0600

    Solve stack with menu

--- ds/06-stack-with-menu/solution.c
+++ ds/06-stack-with-menu/solution.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #define MAX 5
 
-void push(int value);
+void push(int);
 void pop();
 void peek();
 void display();
+int isFull();
+int isEmpty();
 
 int myStack[MAX];
 int top = 0;
@@ -17,10 +19,12 @@ int main(){
     printf("=====================\n");
     printf("1.Push\n2.Pop\n3.Peek\n4.Display\n5.Exit\n\n");
 
+
     while(1){
 
         printf("Enter your choice: ");
         scanf("%d",&choice);
+        printf("\n");
 
         switch(choice){
             case 1:
@@ -49,30 +53,58 @@ int main(){
 
 
 void push(int value){
-    if(top == MAX){
-        printf("Stack if full\n");
+    if(isFull()){
+        printf("Stack if full.\n\n");
     }else{
         myStack[top] = value;
         top++;
+        printf("%d pushed successfully at index %d \n\n", value, top-1);
     }
 }
 
 void pop(){
-
+    if(isEmpty()){
+        printf("Stack is empty.\n\n");
+    }else{
+        int poppedValue = myStack[--top];
+        printf("%d popped successfully from index %d \n\n", poppedValue, top);
+    }
 }
 
 void peek(){
-
+    if(isEmpty()){
+        printf("Stack is empty.\n\n");
+    }else{
+        printf("Top data element of the stack is: %d \n\n", myStack[top-1]);
+    }
 }
 
 void display(){
     int i;
-    if(top == 0){
-        printf("Stack is empty.\n");
-    }
-    else{
+    if(isEmpty()){
+        printf("Stack is empty.\n\n");
+    }else{
         for(i = top - 1; i >= 0; i--){
             printf("myStack[%d] = %d\n",i,myStack[i]);
         }
+        printf("\n");
+    }
+}
+
+int isFull()
+{
+    if(top == MAX){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int isEmpty()
+{
+    if(top == 0){
+        return 1;
+    }else{
+        return 0;
     }
 }
