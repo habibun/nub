6079d2c56b4ef1b89457bb637c6a626ea96e3c5b
UdaySarker <34117689+UdaySarker@users.noreply.github.com>
2018-02-04 13:54:59 +0600

    13 a vowel

---
+++ spl/13-determine-if-a-character-is-vowel/solution.php
@@ -0,0 +1,16 @@
+<?php
+$char=readline("Enter a charcter :\n"); 
+$vowels=array('a','e','i','o','u','A','E','I','O','U');
+$flag=0;
+for($i=0;$i<count($vowels);$i++){
+    if($vowels[$i]==$char){
+        $flag=1;
+    }
+}
+if($flag==1){
+    echo $char. ' is a vowel';
+}
+else{
+    echo $char. ' is not a vowel';
+}
+?>
