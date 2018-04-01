5eab7166b239108024ba09b53ac7da2e89d22c5b
Habibun Noby <habibun.noby@gmail.com>
2018-04-01 09:57:45 +0600

    c++ operator overloading

--- practice/c++/test.cpp
+++ practice/c++/test.cpp
@@ -2,15 +2,35 @@
 using namespace std;
 
 class Test {
+private:
+    int hen;
+    int duck;
 public:
-    Test(int num)
+    Test(int a, int b)
     {
-        cout<<"Fired constructor "<<num;
+        hen = a;
+        duck = b;
+    }
+
+    Test operator + (Test t)
+    {
+        t.hen = t.hen + hen;
+        t.duck = t.duck + duck;
+
+        return t;
+    }
+
+    void show()
+    {
+        cout<<"Total number of hens : "<<hen<<endl;
+        cout<<"Total number of ducks : "<<duck;
     }
 };
 
 
 int main()
 {
-    Test t(7);
+    Test t(5,6), u(7, 8);
+    t = t+u;
+    t.show();
 }
