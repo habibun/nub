e2ad1847d6805c7f0e2a5b114137aa6f334e3db9
Habibun Noby <habibun.noby@gmail.com>
2018-04-05 09:12:06 +0600

    c++ function overriding

---
+++ c++/inheritance/function-overriding-using-virtual-function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class B {
+    public:
+        void show() {
+            cout<<"in base show"<<endl;
+        }
+};
+
+
+class D:public B {
+    public:
+        void show() {
+            cout<<"in derived show"<<endl;
+        }
+};
+
+int main() {
+    B b, *bp;
+    D d;
+    bp=&b;
+    bp->show();
+    bp=&d;
+    bp->show();
+
+    return 0;
+}
