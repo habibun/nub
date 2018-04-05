7c387193032127b9ec9c4179bd5d94037740ac64
Habibun Noby <habibun.noby@gmail.com>
2018-04-05 17:36:59 +0600

    c++ friend function

---
+++ c++/friend-function/friend-function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Add {
+    int x, y, z;
+    public:
+        Add(int, int);
+        friend int calculate(Add p);
+};
+
+Add :: Add(int a, int b) {
+    x=a; y=b;
+}
+int calculate(Add p) {
+    return(p.x+p.y);
+}
+
+int main() {
+    Add a(5, 6);
+    cout<<calculate(a);
+
+return 0;
+}
