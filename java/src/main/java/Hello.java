8fe74f0e0ea4534023f5f74751734d41ee553472
Habibun Noby <habibun.noby@gmail.com>
2019-06-23 23:48:47 +0600

    Add method in java

--- java/src/main/java/Hello.java
+++ java/src/main/java/Hello.java
@@ -9,8 +9,36 @@
  * @author habibun
  */
 public class Hello {
-    public static void main(String []args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        String name = "Jony";
+        int x = 10;
+        int y = 5, z = 10;
+        char singleCharacter = 'S';
+        boolean b = true;
+        float de = 19.4f;
+        String message = "Hello world";
+        String messageLength = "Hello world";
+        String firstName = "John";
+        String lastName = "Doe";
+        String text = "it's my name";
+        String[] car = {"t", "b"};
+//        System.out.println("Hello " + name + " " + z + " " + firstName.concat(lastName) + " " + car[0]);
+//        MyNameWithParam("jony");
+        
+        int[] testArr = {4, 5, 6, 10};
+        for(int i = 0; i < testArr.length; i++) {
+            System.out.printf("The value of testArr[%d] is %d \n", i, testArr[i]);
+        }
+    }
+    
+    public static void myName()
+    {
+        System.out.println("My name is jony!");
+    }
+    
+    public static void MyNameWithParam(String name)
+    {
+        System.out.println("My name is " + name);
     }
 }
